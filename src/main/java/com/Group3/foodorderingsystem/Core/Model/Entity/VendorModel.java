c54e8bba0c3eae04175fa4f8bb94a64ba2828c98
WL <limweilun3838@gmail.com>
2024-12-02 16:48:53 +0800

    Added Util for file saving mechanism

--- src/main/java/com/Group3/foodorderingsystem/Core/Model/Entity/VendorModel.java
+++ src/main/java/com/Group3/foodorderingsystem/Core/Model/Entity/VendorModel.java
@@ -2,6 +2,7 @@ package com.Group3.foodorderingsystem.Core.Model.Entity;
 
 import com.Group3.foodorderingsystem.Core.Model.Abstract.User;
 import com.Group3.foodorderingsystem.Core.Model.Enum.RoleEnum;
+import com.Group3.foodorderingsystem.Core.Model.Enum.StatusEnum;
 
 public class VendorModel extends User {
     
@@ -11,6 +12,7 @@ public class VendorModel extends User {
 
     private double revenue; 
     private String address;
+    private StatusEnum status; // NOTE: CHECK NEED THIS OR NOT, ADMIN WILL NEED TO APPROVE THE VENDOR DOCS/LICENSE BEFORE APPROVAL
 
     public double getRevenue() {
         return revenue;
@@ -20,12 +22,20 @@ public class VendorModel extends User {
         return address;
     }
 
+    public StatusEnum getStatus() {
+        return status;
+    }
+
     public void setRevenue(double revenue) {
         this.revenue = revenue;
     }
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
     }
 
 }
---
+++ src/main/java/com/Group3/foodorderingsystem/Core/Model/Enum/StatusEnum.java
@@ -0,0 +1,8 @@
+package com.Group3.foodorderingsystem.Core.Model.Enum;
+
+public enum StatusEnum {
+    
+    PENDING,
+    ACCEPTED, 
+    REJECTED; 
+}
