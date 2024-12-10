661c2f3f8dc7b81fd8df26be8df63d4b83cb9f8f
WL <limweilun3838@gmail.com>
2024-12-10 10:27:18 +0800

    Fixing bugs in itemservice and fileutil

--- src/main/java/com/Group3/foodorderingsystem/Core/Services/ItemServices.java
+++ src/main/java/com/Group3/foodorderingsystem/Core/Services/ItemServices.java
@@ -3,17 +3,25 @@ package com.Group3.foodorderingsystem.Core.Services;
 import java.util.List;
 
 import com.Group3.foodorderingsystem.Core.Model.Entity.ItemModel;
+import com.Group3.foodorderingsystem.Core.Storage.Storage;
 import com.Group3.foodorderingsystem.Core.Storage.StorageEnum;
 import com.Group3.foodorderingsystem.Core.Util.FileUtil;
 
 public class ItemServices {
-    
+
     public static List<ItemModel> getItems() {
-        return FileUtil.loadFile(StorageEnum.getFileName(StorageEnum.ITEM), ItemModel.class); 
+        return FileUtil.loadFile(StorageEnum.getFileName(StorageEnum.ITEM), ItemModel.class);
     }
 
+    // save new or modify existing, use this
     public static ItemModel saveItem(ItemModel itemModel) {
         List<ItemModel> items = getItems();
+
+        if (itemModel.getItemId() == null)
+            itemModel.setItemId(Storage.generateNewId());
+        else
+            items.removeIf(item -> item.getItemId().equals(itemModel.getItemId()));
+
         items.add(itemModel);
 
         FileUtil.saveFile(StorageEnum.getFileName(StorageEnum.ITEM), items);
