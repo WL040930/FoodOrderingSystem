5acdbb1fd0876017bfbcf96eab6939a69db6e64c
LIM GEN JACK <135495371+Jack-1118@users.noreply.github.com>
2025-01-04 15:24:41 +0800

    Update VendorNavigationEnum.java

---
+++ src/main/java/com/Group3/foodorderingsystem/Module/Platform/Vendor/Assets/VendorNavigationEnum.java
@@ -0,0 +1,52 @@
+package com.Group3.foodorderingsystem.Module.Platform.Vendor.Assets;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum VendorNavigationEnum {
+
+    Home(
+            "Home",
+            "logo.png",
+            Arrays.asList(
+                    VendorTopNavigationEnum.Home)),
+    Shop(
+            "Shop",
+            "shop_icon.png",
+            Arrays.asList(
+                    VendorTopNavigationEnum.Menu, 
+                    VendorTopNavigationEnum.Review)),
+    Notification(
+            "Notification",
+            "logo.png",
+            Arrays.asList(
+                    VendorTopNavigationEnum.Notification)),
+    Settings(
+            "Settings",
+            "settings.png",
+            Arrays.asList(
+                    VendorTopNavigationEnum.Settings, 
+                    VendorTopNavigationEnum.Revenue));
+
+    private final String title;
+    private final String icon;
+    private final List<VendorTopNavigationEnum> topNavigationItem;
+
+    VendorNavigationEnum(String title, String icon, List<VendorTopNavigationEnum> topNavigationItem) {
+        this.title = title;
+        this.icon = icon;
+        this.topNavigationItem = topNavigationItem;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public List<VendorTopNavigationEnum> getTopNavigationItem() {
+        return topNavigationItem;
+    }
+}
